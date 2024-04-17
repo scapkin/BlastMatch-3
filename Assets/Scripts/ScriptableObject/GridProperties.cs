56a761c2f30e484b5db3cb78a115f8c874e71f0d
scapkin <seyfullahcapkin@ogrenci.beykoz.edu.tr>
2024-04-17 16:29:28 +0300

    Game properties system added

---
+++ Assets/Scripts/ScriptableObject/GridProperties.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using Pool;
+using UnityEngine;
+
+namespace ScriptableObject
+{
+    [CreateAssetMenu(fileName = "GridProperties", menuName = "GridProperties", order = 0)]
+    public class GridProperties : UnityEngine.ScriptableObject
+    {
+        public int GridSize = 8;
+        public List<Gem> Gems;
+        public int MoveCount = 30;
+        public GameObject GridBackgroundObject;
+        public float Space = 0.65f;
+        [Serializable]
+        public class Gem
+        {
+            public ObjectPoolItem.GemType Type;
+            public int Goal;
+        }
+    }
+}
