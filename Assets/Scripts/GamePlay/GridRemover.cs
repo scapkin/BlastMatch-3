829bac0b04d7a8d70cddc4dfd77384d79e1801c3
scapkin <seyfullahcapkin@ogrenci.beykoz.edu.tr>
2024-04-17 23:51:23 +0300

    Bomb create bug fixed

--- Assets/Scripts/GamePlay/GridRemover.cs
+++ Assets/Scripts/GamePlay/GridRemover.cs
@@ -10,16 +10,23 @@ namespace GamePlay
     {
         public static void RemoveGridObject(Cell[,] gridArray,List<Cell> gridObjects,int x,int y)
         {
+            if (gridObjects.Count>5)
+            {
+                gridObjects.Remove(gridArray[x,y]);
+                gridArray[x,y].GridObject.isCheck = false;
+                ObjectPool.Instance.ReturnObjectToPool(gridArray[x,y].GridObject.gameObject);
+                gridArray[x,y] = null;
+                
+                GridSpawner.CreateNewGridAction?.Invoke(x,y, ObjectPoolItem.GemType.Bomb);
+                
+            }
             foreach (var gridObject in gridObjects)
             {
                 gridArray[gridObject.GridObject.PosX, gridObject.GridObject.PosY] = null;
                 gridObject.GridObject.isCheck = false;
                 ObjectPool.Instance.ReturnObjectToPool(gridObject.GridObject.gameObject);
             }
-            if (gridObjects.Count>5)
-            {
-                GridSpawner.CreateNewGridAction?.Invoke(x,y, ObjectPoolItem.GemType.Bomb);
-            }
+            
 
             GridDropManager.Drop(gridArray,gridObjects);
         }
